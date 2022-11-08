01d43ea6d71d187b0b6795bccb7f3a150315139d
Weber❤️H2O2 <82763013+Weber-H2O2@users.noreply.github.com>
2022-11-08 22:55:31 +0800

    test: modify code according to the changement of apis

--- scripts/run_with_eigen_service.js
+++ scripts/run_with_eigen_service.js
@@ -17,13 +17,18 @@ const digest = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("Hello Eigen"));
 
 async function keygen(m, delay) {
   user_id = 1;
-  keygen_url = `${eigen_service}/user/${user_id}/tss/keygen`;
-  res = await axios.post(keygen_url, {
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-  });
-  console.log(res);
+  key_name = "Just test";
+  keygen_url = `${eigen_service}/tss/keygen`;
+  res = await axios.post(
+    keygen_url,
+    querystring.stringify({ user_id: user_id, name: key_name }),
+    {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    }
+  );
+  console.log(res.data);
   await delay_ms(1000);
 
   let context = await m.gg18_keygen_client_new_context(addr, t, n, delay);
@@ -31,60 +36,80 @@ async function keygen(m, delay) {
   console.log("keygen new context: ");
 
   round = 1;
-  res = await axios.post(keygen_url, querystring.stringify({ round: round }), {
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-  });
-  console.log(res);
+  res = await axios.post(
+    keygen_url,
+    querystring.stringify({ user_id: user_id, round: round }),
+    {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    }
+  );
+  console.log(res.data);
   await delay_ms(1000);
 
   context = await m.gg18_keygen_client_round1(context, delay);
   console.log("keygen round1:");
 
   round = 2;
-  res = await axios.post(keygen_url, querystring.stringify({ round: round }), {
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-  });
-  console.log(res);
+  res = await axios.post(
+    keygen_url,
+    querystring.stringify({ user_id: user_id, round: round }),
+    {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    }
+  );
+  console.log(res.data);
   await delay_ms(1000);
 
   context = await m.gg18_keygen_client_round2(context, delay);
   console.log("keygen round2: ");
 
   round = 3;
-  res = await axios.post(keygen_url, querystring.stringify({ round: round }), {
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-  });
-  console.log(res);
+  res = await axios.post(
+    keygen_url,
+    querystring.stringify({ user_id: user_id, round: round }),
+    {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    }
+  );
+  console.log(res.data);
   await delay_ms(1000);
 
   context = await m.gg18_keygen_client_round3(context, delay);
   console.log("keygen round3: ");
 
   round = 4;
-  res = await axios.post(keygen_url, querystring.stringify({ round: round }), {
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-  });
-  console.log(res);
+  res = await axios.post(
+    keygen_url,
+    querystring.stringify({ user_id: user_id, round: round }),
+    {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    }
+  );
+  console.log(res.data);
   await delay_ms(1000);
 
   context = await m.gg18_keygen_client_round4(context, delay);
   console.log("keygen round4: ");
 
   round = 5;
-  res = await axios.post(keygen_url, querystring.stringify({ round: round }), {
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-  });
-  console.log(res);
+  res = await axios.post(
+    keygen_url,
+    querystring.stringify({ user_id: user_id, round: round }),
+    {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    }
+  );
+  console.log(res.data);
   await delay_ms(1000);
 
   keygen_json = await m.gg18_keygen_client_round5(context, delay);
@@ -99,12 +124,13 @@ async function keygen(m, delay) {
 async function sign(m, key_store, delay, public_key_address) {
   console.log("public_key_address = ", public_key_address);
   user_id = 1;
-  sign_url = `${eigen_service}/user/${user_id}/tss/sign`;
+  sign_url = `${eigen_service}/tss/sign`;
   res = await axios.post(
     sign_url,
     querystring.stringify({
       digest: digest.slice(2),
       user_address: public_key_address,
+      user_id: user_id,
     }),
     {
       headers: {
@@ -112,7 +138,7 @@ async function sign(m, key_store, delay, public_key_address) {
       },
     }
   );
-  console.log(res);
+  console.log(res.data);
   await delay_ms(1000);
 
   let context = await m.gg18_sign_client_new_context(
@@ -125,120 +151,160 @@ async function sign(m, key_store, delay, public_key_address) {
   console.log("sign new context: ", context);
 
   round = 0;
-  res = await axios.post(sign_url, querystring.stringify({ round: round }), {
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-  });
-  console.log(res);
+  res = await axios.post(
+    sign_url,
+    querystring.stringify({ user_id: user_id, round: round }),
+    {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    }
+  );
+  console.log(res.data);
   await delay_ms(1000);
 
   context = await m.gg18_sign_client_round0(context, delay);
   console.log("sign round0: ");
 
   round = 1;
-  res = await axios.post(sign_url, querystring.stringify({ round: round }), {
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-  });
-  console.log(res);
+  res = await axios.post(
+    sign_url,
+    querystring.stringify({ user_id: user_id, round: round }),
+    {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    }
+  );
+  console.log(res.data);
   await delay_ms(1000);
 
   context = await m.gg18_sign_client_round1(context, delay);
   console.log("sign round1: ");
 
   round = 2;
-  res = await axios.post(sign_url, querystring.stringify({ round: round }), {
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-  });
-  console.log(res);
+  res = await axios.post(
+    sign_url,
+    querystring.stringify({ user_id: user_id, round: round }),
+    {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    }
+  );
+  console.log(res.data);
   await delay_ms(1000);
 
   context = await m.gg18_sign_client_round2(context, delay);
   console.log("sign round2: ");
 
   round = 3;
-  res = await axios.post(sign_url, querystring.stringify({ round: round }), {
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-  });
-  console.log(res);
+  res = await axios.post(
+    sign_url,
+    querystring.stringify({ user_id: user_id, round: round }),
+    {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    }
+  );
+  console.log(res.data);
   await delay_ms(1000);
 
   context = await m.gg18_sign_client_round3(context, delay);
   console.log("sign round3: ");
 
   round = 4;
-  res = await axios.post(sign_url, querystring.stringify({ round: round }), {
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-  });
-  console.log(res);
+  res = await axios.post(
+    sign_url,
+    querystring.stringify({ user_id: user_id, round: round }),
+    {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    }
+  );
+  console.log(res.data);
   await delay_ms(1000);
 
   context = await m.gg18_sign_client_round4(context, delay);
   console.log("sign round4: ");
 
   round = 5;
-  res = await axios.post(sign_url, querystring.stringify({ round: round }), {
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-  });
-  console.log(res);
+  res = await axios.post(
+    sign_url,
+    querystring.stringify({ user_id: user_id, round: round }),
+    {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    }
+  );
+  console.log(res.data);
   await delay_ms(1000);
 
   context = await m.gg18_sign_client_round5(context, delay);
   console.log("sign round5: ");
 
   round = 6;
-  res = await axios.post(sign_url, querystring.stringify({ round: round }), {
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-  });
-  console.log(res);
+  res = await axios.post(
+    sign_url,
+    querystring.stringify({ user_id: user_id, round: round }),
+    {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    }
+  );
+  console.log(res.data);
   await delay_ms(1000);
 
   context = await m.gg18_sign_client_round6(context, delay);
   console.log("sign round6: ");
 
   round = 7;
-  res = await axios.post(sign_url, querystring.stringify({ round: round }), {
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-  });
-  console.log(res);
+  res = await axios.post(
+    sign_url,
+    querystring.stringify({ user_id: user_id, round: round }),
+    {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    }
+  );
+  console.log(res.data);
   await delay_ms(1000);
 
   context = await m.gg18_sign_client_round7(context, delay);
   console.log("sign round7: ");
 
   round = 8;
-  res = await axios.post(sign_url, querystring.stringify({ round: round }), {
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-  });
-  console.log(res);
+  res = await axios.post(
+    sign_url,
+    querystring.stringify({ user_id: user_id, round: round }),
+    {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    }
+  );
+  console.log(res.data);
   await delay_ms(1000);
 
   context = await m.gg18_sign_client_round8(context, delay);
   console.log("sign round8: ");
 
   round = 9;
-  res = await axios.post(sign_url, querystring.stringify({ round: round }), {
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-  });
-  console.log(res);
+  res = await axios.post(
+    sign_url,
+    querystring.stringify({ user_id: user_id, round: round }),
+    {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    }
+  );
+  console.log(res.data);
   await delay_ms(1000);
 
   sign_json = await m.gg18_sign_client_round9(context, delay);
